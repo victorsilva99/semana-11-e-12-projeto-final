baad489140b3ece46d4786161896f470b275f329
Victor Silva de Souza <fe605@t-systems.com.br>
2022-03-23 10:18:31 -0300

    Initial Commit

---
+++ TSystems.ASPNETCoreMVC.Torneio/TSystems.ASPNETCoreMVC.Torneio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TSystems.ASPNETCoreMVC.Torneio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
