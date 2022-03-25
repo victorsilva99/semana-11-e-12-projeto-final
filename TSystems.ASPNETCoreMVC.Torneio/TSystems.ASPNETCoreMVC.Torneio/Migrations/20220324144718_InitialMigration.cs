c02593faa01fb7416baf324c22bdae5dfc93d5b5
Victor Silva de Souza <fe605@t-systems.com.br>
2022-03-25 08:07:17 -0300

    [FEAT] Criado Migrations de inserção de dados

---
+++ TSystems.ASPNETCoreMVC.Torneio/TSystems.ASPNETCoreMVC.Torneio/Migrations/20220324144718_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TSystems.ASPNETCoreMVC.Torneio.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Champions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    Habilidades = table.Column<int>(type: "int", nullable: false),
+                    TotalDeLutas = table.Column<int>(type: "int", nullable: false),
+                    Vitorias = table.Column<int>(type: "int", nullable: false),
+                    Derrotas = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Champions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Champions");
+        }
+    }
+}
---
+++ TSystems.ASPNETCoreMVC.Torneio/TSystems.ASPNETCoreMVC.Torneio/Migrations/20220324150921_PopularChampions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TSystems.ASPNETCoreMVC.Torneio.Migrations
+{
+    public partial class PopularChampions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Champions(Nome, Idade, Habilidades, TotalDeLutas, Vitorias, Derrotas)" +
+             "VALUES ('Ashe', 21, 4, 0, 0, 0)," +
+                    "('Azir', 39, 4, 0, 0, 0)," +
+                    "('Ezreal', 21, 4, 0, 0, 0)," +
+                    "('Jarvan', 41, 4, 0, 0, 0)," +
+                    "('Jax', 38, 4, 0, 0, 0)," +
+                    "('Jayce', 27, 4, 0, 0, 0)," +
+                    "('Jinx', 20, 4, 0, 0, 0)," +
+                    "('Kassadin', 37, 4, 0, 0, 0)," +
+                    "('Kassandra', 39, 4, 0, 0, 0)," +
+                    "('Kindred', 40, 4, 0, 0, 0)," +
+                    "('Lee Sin', 37, 4, 0, 0, 0)," +
+                    "('Rengar', 55, 4, 0, 0, 0)," +
+                    "('Shaco', 22, 4, 0, 0, 0)," +
+                    "('Shyvana', 35, 4, 0, 0, 0)," +
+                    "('Sona', 21, 4, 0, 0, 0)," +
+                    "('Thresh', 78, 4, 0, 0, 0)," +
+                    "('Twisted Fate', 24, 4, 0, 0, 0)," +
+                    "('Vladimir', 27, 4, 0, 0, 0)," +
+                    "('Yasuo', 28, 4, 0, 0, 0)," +
+                    "('Zed', 38, 4, 0, 0, 0)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
