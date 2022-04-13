1a44158e89acf11a4d5b7ccb8b97509602cb510c
Victor Silva <fe605@t-systems.com.br>
2022-04-13 00:40:07 -0300

    [FEAT] Adicionado sons nas views index e vencedor

--- TSystems.ASPNETCoreMVC.Torneio/TSystems.ASPNETCoreMVC.Torneio/wwwroot/js/pages/sounds.js
+++ TSystems.ASPNETCoreMVC.Torneio/TSystems.ASPNETCoreMVC.Torneio/wwwroot/js/pages/sounds.js
@@ -1,8 +1,14 @@
-﻿var audioClick = new Audio('/medias/audios/champion-select.mp3');
-var audioBackground = new Audio('/medias/audios/background-sound.mp3');
+﻿var audioClick = new Audio('/medias/audios/champion-select-sound.mp3');
+var audioBackgroundIndex = new Audio('/medias/audios/index-background-sound.mp3');
+var audioBackgroundVencedor = new Audio('/medias/audios/vencedor-background-sound.mp3');
+var audioBackgroundChampions = new Audio('/medias/audios/champions-backgound-sound.mp3');
+var audioVictory = new Audio('/medias/audios/victory-voice-sound.mp3');
 
 function diminuirVolumeGeral()
 {
     audioClick.volume = 0.05;
-    audioBackground.volume = 0.05;
+    audioBackgroundIndex.volume = 0.05;
+    audioBackgroundVencedor.volume = 0.05;
+    audioBackgroundChampions.volume = 0.05;
+    audioVictory.volume = 0.05;
 }
